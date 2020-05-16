5d1044e56a8d7205dcc884d88ccf99c91836e92c
telzhou618 <telzhou618@qq.com>
2020-05-16 23:30:07 +0800

    eureka 注册中心 update

--- src/main/java/com/example/EurekaServerApplication.java
+++ src/main/java/com/example/EurekaServerApplication.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author gaojun.zhou
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
